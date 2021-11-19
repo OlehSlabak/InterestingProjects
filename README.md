db796879b6e5ed2fd4f77d6e35f6a5a70f758978
Oleh Slabak <pesekot@gmail.com>
2021-11-19 15:41:14 +0200

    set, get and set_all == O(1)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-first commit
+set, get, set_all - Do SET, GET and SET ALL operations by O(1) complexity.
+
+
--- set, get, set_all/main.cpp
+++ set, get, set_all/main.cpp
@@ -1,6 +1,46 @@
-#include <iostream>
+#include <stdio.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+struct Arr
+{
+    int elem;
+    size_t time_local;
+};
+
+size_t time_global = 1;
+int global_value = 0;
+size_t time_global_all = 0;
+
+int get(struct Arr a[], int i)
+{
+    if(a[i].time_local > time_global_all)
+        return a[i].elem;
+    else
+        return global_value;
+}
+
+void set(struct Arr *a, int j)
+{
+    a->elem = j;
+    time_global++;
+    a->time_local = time_global;
+}
+
+void setAll(int x)
+{
+    time_global_all = time_global;
+    global_value = x;
+}
+
+int main()
+{
+    struct Arr temp[5] = {0, 0};
+    set(&temp[1], 3);
+    setAll(6);
+    printf(" %d \n", get(temp, 1));
+    set(&temp[2], 11);
+    printf(" %d \n", get(temp, 2));
+    setAll(5);
+    printf(" %d \n", get(temp, 2));
+
     return 0;
 }
